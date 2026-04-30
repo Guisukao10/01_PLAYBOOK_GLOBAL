ab4d8a1bbebc21b653dd3cf52c93449bf45e9916
Guilherme Bernardes Alamino <guilherme.alamino@alliagedc.local>
2026-04-30 16:48:33 -0300

    Refatora módulos4

--- 01_KPI/KPI_V2/js/index.js
+++ 01_KPI/KPI_V2/js/index.js
@@ -1,5 +1,5 @@
 const POWER_BI_URL =
-    "https://app.powerbi.com/view?r=eyJrIjoiMGVmMTQwYjMtZDI5OS00NWRhLTk2M2QtZTgwNzY3NWM2YmZiIiwidCI6ImE2NzRhMDgxLTBjNTMtNGQyZC1hZWQ2LWRiZjgwNmY5NWExYiJ9";
+    "https://app.powerbi.com/view?r=eyJrIjoiNTk3MDk4MmUtMWI0OC00MWI1LTg1OTYtMTFjZGE3ZGRkODIzIiwidCI6ImE2NzRhMDgxLTBjNTMtNGQyZC1hZWQ2LWRiZjgwNmY5NWExYiJ9";
 
 document.addEventListener("DOMContentLoaded", function () {
     const frame = document.getElementById("dashboardBiFrame");
